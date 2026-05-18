4c088ac6fdb358dad6644f9a9d98d3f76ecb23ac
shaikmyshataskeen2008-dot <shaikmyshataskeen2008@gmail.com>
2026-05-18 12:26:09 +0000

    one more change

--- Bank/src/Even.java
+++ Bank/src/Even.java
@@ -1,4 +1,14 @@
 
 public class Even {
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+		System.out.println("Even numbers in the array:");
+		for (int number : numbers) {
+			if (number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+	}
 }
+
