c17e826d83a3029326455539f75ba36ac5441981
shaikmyshataskeen2008-dot <shaikmyshataskeen2008@gmail.com>
2026-05-19 04:10:17 +0000

    one more has been added

---
+++ Bank/src/PositiveNegative.java
@@ -0,0 +1,20 @@
+
+public class PositiveNegative {
+	public static void main(String[] args) {
+		int[] numbers = {-5, -3, -1, 0, 1, 3, 5};
+
+		System.out.println("Positive numbers in the array:");
+		for (int number : numbers) {
+			if (number > 0) {
+				System.out.println(number);
+			}
+		}
+
+		System.out.println("Negative numbers in the array:");
+		for (int number : numbers) {
+			if (number < 0) {
+				System.out.println(number);
+			}
+		}
+	}
+}
