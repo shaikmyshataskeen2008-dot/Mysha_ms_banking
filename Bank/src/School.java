01d57cf055aaecdaa861206be8146a55986f2904
shaikmyshataskeen2008-dot <shaikmyshataskeen2008@gmail.com>
2026-05-19 05:55:22 +0000

    added

---
+++ Bank/src/School.java
@@ -0,0 +1,14 @@
+
+public class School {
+	public static void main(String[] args) {
+		String schoolName = "Greenwood High School";
+		int numberOfStudents = 500;
+		String principalName = "Mr. Smith";
+
+		System.out.println("School Name: " + schoolName);
+		System.out.println("Number of Students: " + numberOfStudents);
+		System.out.println("Principal Name: " + principalName);
+		System.out.println("Welcome to " + schoolName + "!");	
+	}
+
+}
