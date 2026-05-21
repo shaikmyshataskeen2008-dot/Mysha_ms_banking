374c90d8dae1f07884049c7e224a28636e0bbf09
shaikmyshataskeen2008-dot <shaikmyshataskeen2008@gmail.com>
2026-05-21 05:22:27 +0000

    Added

---
+++ Employee/src/com/employee/model/school.java
@@ -0,0 +1,26 @@
+package com.employee.model;
+
+public class school {
+	private String schoolName;
+	private String schoolAddress;
+	private String schoolPhone;
+	public String getSchoolName() {
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	public String getSchoolAddress() {
+		return schoolAddress;
+	}
+	public void setSchoolAddress(String schoolAddress) {
+		this.schoolAddress = schoolAddress;
+	}
+	public String getSchoolPhone() {
+		return schoolPhone;
+	}
+	public void setSchoolPhone(String schoolPhone) {
+		this.schoolPhone = schoolPhone;
+	}
+
+}
